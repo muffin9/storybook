4aa648e46ee3a6c8899a7198bed9b9f49c4c661a
Muffin <jinlog9@gmail.com>
2022-05-08 18:47:24 +0900

    feat: msw 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function App(): JSX.Element {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('/test')
+        .then(response => response.json())
+        .then(data => console.log(data));
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div>Muffin's boilerPlate!</div>
