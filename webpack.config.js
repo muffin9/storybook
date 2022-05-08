b64f89bbb78e6f128dc2c4cdd5870f252a9d4d11
Muffin <jinlog9@gmail.com>
2022-05-08 18:46:19 +0900

    build: process.env 개발 환경일때 development로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   mode,
   entry: {
-    app: path.join(__dirname, "src", "index.tsx"),
+    app: path.join(__dirname, 'src', 'index.tsx'),
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          process.env.NODE_ENV === "production"
+          process.env.NODE_ENV === 'production'
             ? MiniCssExtractPlugin.loader
-            : "style-loader",
-          "css-loader",
+            : 'style-loader',
+          'css-loader',
         ],
       },
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       templateParameters: {
-        env: process.env.NODE_ENV === "production" ? "" : "[DEV]",
+        env: process.env.NODE_ENV === 'production' ? '' : 'development',
       },
       minify:
-        process.env.NODE_ENV === "production"
+        process.env.NODE_ENV === 'production'
           ? {
               collapseWhitespace: true,
               removeComments: true,
@@ -51,8 +51,8 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       openAnalyzer: false,
-      analyzerMode: "static",
-      reportFilename: "./report.html",
+      analyzerMode: 'static',
+      reportFilename: './report.html',
     }),
   ],
 };
